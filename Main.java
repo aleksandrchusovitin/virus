6fc75c4ebe475d9ca7b1b9d0e0ed44b31a292f65
nerealist <38918351+nerealist@users.noreply.github.com>
2019-08-20 17:56:43 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * created by Aleksandr Chusovitin 10/07/2019
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+}
